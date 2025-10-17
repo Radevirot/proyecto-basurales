228bce10f6c790833e1d762a5894ec2cc14f7fbe
Radevirot <78053632+Radevirot@users.noreply.github.com>
2025-10-16 21:53:09 -0300

    .

--- flask/csv_test.py
+++ flask/csv_test.py
@@ -1,28 +1,27 @@
-# import csv
-# import os
+import csv
+import os
 
-# campos = ['nombre','edad','dni']
-# filas = [
-#     ['Nombre1', '24', '42926969'],
-#     ['Nombre2', '24', '29383523'],
-#     ['Nombre3', '25', '42555838'],
-#     ['Nombre4', '32', '43775386']
-# ]
+campos = ['nombre','edad','dni']
+filas = [
+    ['Nombre1', '24', '42926969'],
+    ['Nombre2', '24', '29383523'],
+    ['Nombre3', '25', '42555838'],
+    ['Nombre4', '32', '43775386']
+]
 
-# nombre_carpeta = 'CSVs'
+nombre_carpeta = 'CSVs'
 
-# os.makedirs(nombre_carpeta, exist_ok=True)
+os.makedirs(nombre_carpeta, exist_ok=True)
 
-# nombre_archivo = os.path.join(nombre_carpeta, "lista_personas.csv")
+nombre_archivo = os.path.join(nombre_carpeta, "lista_personas.csv")
 
-# with open(nombre_archivo, mode='w', newline='', encoding='utf-8') as csvfile:
-#     csvwriter = csv.writer(csvfile)
-#     csvwriter.writerow(campos)
-#     csvwriter.writerows(filas)
+with open(nombre_archivo, mode='w', newline='', encoding='utf-8') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(campos)
+    csvwriter.writerows(filas)
     
-import secrets
-print(secrets.token_hex(32))  # 64 hex chars
-    
+# import secrets
+# print(secrets.token_hex(32))  # 64 hex chars
     
     
     
