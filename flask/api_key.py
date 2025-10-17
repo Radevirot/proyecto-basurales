b6260519d45a1df4df4c5986165da279b6017dfb
Radevirot <78053632+Radevirot@users.noreply.github.com>
2025-10-16 21:49:36 -0300

    whitelist implementada, .env implementado

--- flask/api_key.py
+++
@@ -1,2 +0,0 @@
-CLIENT_ID = "678709209239-ggavbt304u6rrqpsd23n66im71hhj0fu.apps.googleusercontent.com"
-CLIENT_SECRET = "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"
--- flask/app.py
+++ flask/app.py
@@ -4,16 +4,28 @@ import json
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from authlib.integrations.flask_client import OAuth
-from api_key import * #usar variables de entorno en deployment
+from dotenv import load_dotenv #usar variables de entorno en deployment
 
 # --------------------
 # Configuración
 # --------------------
 
-CSV_USUARIOS = "usuarios.csv"
+load_dotenv("keys.env")
+
+CSV_WHITELIST = "whitelist.csv"
 
 app = Flask(__name__)
-app.secret_key = "7Abcet3dsd2F.sfw-Gwa2" #meter esto en las variables de entorno
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
+
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+
+if not CLIENT_ID or not CLIENT_SECRET:
+    raise RuntimeError(
+    "Faltan CLIENT_ID o CLIENT_SECRET en las variables de entorno. "
+    )
 
 oauth = OAuth(app)
 
@@ -31,6 +43,7 @@ google = oauth.register(
 # ----------------------------------------
 # Funciones auxiliares para manejo de CSVs
 # ----------------------------------------
+
 def existe_csv(ruta, cabeceras):
 #Crea un CSV con cabecera si no existe
     if not os.path.exists(ruta):
@@ -38,41 +51,56 @@ def existe_csv(ruta, cabeceras):
             writer = csv.writer(f)
             writer.writerow(cabeceras)
 
+# SISTEMA DE WHITELIST
 
-def actualizar_usuario(ruta_csv, info_usuario):
-#actualiza o inserta una fila nueva en usuarios.csv
+def leer_whitelist():
+#leemos el csv de la whitelist y retornamos un diccionario con sus valores
+    existe_csv(CSV_WHITELIST, ["correo"])
+    valores = []
+    with open(CSV_WHITELIST, "r", newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            v = (row.get("correo") or "").strip() # or "" en caso de que la whitelist tenga valores que podrían ser falsos tipo "None" o "0"
+                                                  # strip le saca los espacios antes y después
+            if not v: #checkeamos si v es "", y si es lo salteamos
+                continue
+            valores.append(v)
+    return valores
 
-    existe_csv(ruta_csv, ["email", "ultimo_login"])
-    # cargo las filas de "usuarios.csv" en el diccionario "filas"
-    filas = []
-    actualizado = False
-    with open(ruta_csv, "r", newline="", encoding="utf-8") as f:
-        lector = csv.DictReader(f)
-        for fila in lector:
-            filas.append(fila)
 
-    fecha = datetime.now().isoformat()
-    for fila in filas:
-        if fila.get("email", "") == info_usuario.get("email"):
-            # si el mail ya está en el csv, actualizo la fecha del ultimo logeo
-            fila["ultimo_login"] = fecha
-            actualizado = True
-            break
+def esta_permitido(email):
+    whitelist = leer_whitelist()
+    #si la whitelist está vacía permitimos todos los usuarios
+    if not whitelist:
+        return True, "no hay whitelist configurada, se permiten todos los usuarios"
 
-    if not actualizado:
-        # si el mail no estaba, agrego el usuario nuevo
-        filas.append({
-        "email": info_usuario.get("email", ""),
-        "ultimo_login": fecha,
-        })
+    #reviso si hay coincidencia exacta
+    for item in whitelist:
+        if email.lower() == item.lower():
+            return True, f"coincidencia: {item}"
 
-# sobreescribimos el csv con el diccionario "filas" nuevo
-    with open(ruta_csv, "w", newline="", encoding="utf-8") as f:
-        cabeceras = ["email", "ultimo_login"]
-        writer = csv.DictWriter(f, fieldnames=cabeceras)
-        writer.writeheader()
-        for fila in filas:
-            writer.writerow(fila)
+    return False, "no está en la whitelist"
+
+# HISTORIAL DE LOGEO DE USUARIOS
+
+def guardar_login_usuario(email):
+ #guarda la fecha y hora del login en un archivo CSV individual en de la carpeta login_data.
+    
+    carpeta = "login_data"
+    os.makedirs(carpeta, exist_ok=True)  # crea la carpeta si no existe
+    permitido = esta_permitido(email)
+    
+    # me quedo con lo que está antes del .
+    nombre_archivo = email.partition(".")[0] + ".csv" 
+    ruta_archivo = os.path.join(carpeta, nombre_archivo)
+
+    # Si el archivo no existe, lo creamos con encabezado y luego agregamos la fila con el last_login
+    # y un campo que nos diga si el usuario está permitido o no en el sistema
+    existe_csv(ruta_archivo,["last_login", "allowed"])
+    with open(ruta_archivo, "a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow([datetime.now().isoformat(), permitido[0]])
+
 
 # -----------------------
 # Rutas de la página web
@@ -81,9 +109,17 @@ def actualizar_usuario(ruta_csv, info_usuario):
 @app.route("/")
 def index():
     #verificamos si hay una sesión buscando el correo, si hay mostramos cerrar sesión y si no iniciarla
+    #si el correo existe verificamos que esté en la whitelist
     user_email = session.get("email")
+    
     if user_email:
-        return f"Hola {user_email}! <a href='/logout'>Cerrar sesión</a>"
+        permitido = esta_permitido(user_email)
+        if permitido[0]:
+            username = session.get("nombre")
+            return f"¡Hola {username}! ¡Tu correo ({user_email}) está permitido! <a href='/logout'>Cerrar sesión</a>"
+            #return render_template("index.html", email=user_email, username=username)
+        else:
+            return f"¡Hola {user_email}! Tu correo ({user_email}) NO está permitido en esta página, por favor cierra sesión. <a href='/logout'>Cerrar sesión</a>"
     return "<a href='/login/google'>Iniciar sesión con Google</a>"
 
 
@@ -99,18 +135,22 @@ def login_google():
 
 @app.route("/authorize/google")
 def authorize_google():
-    token = google.authorize_access_token()
-    session["token"] = token #me guardo el token de google en la sesión de flask
+    google.authorize_access_token() #no me guardo el token porque no lo necesito
     userinfo_endpoint = google.server_metadata['userinfo_endpoint']
     res = google.get(userinfo_endpoint)
     info_usuario = res.json()
+    # me guardo correo, nombre e imagen del usuario
     email = info_usuario['email']
+    nombre = info_usuario['name']
+    foto = info_usuario['picture']
     
-    # guardamos al usuario o actualizamos su fecha
-    actualizar_usuario(CSV_USUARIOS, {"email": email})
+    # agregamos fecha nueva de logeo para el usuario
+    guardar_login_usuario(email)
 
-    # guardamos el correo en la sesión
+    # guardamos el correo, nombre y foto en la sesión de flask, que dura hasta que se cierre el navegador
     session["email"] = email
+    session["nombre"] = nombre
+    session["foto"] = foto
 
     return redirect(url_for("index"))
 
@@ -124,8 +164,8 @@ def logout():
 @app.route("/debug")
 def debug():
     #esta ruta está para printear cosas del desarrollo, solo de prueba
-    print(session)  
-    return f"<pre>{json.dumps(dict(session), indent=2)}</pre>"
+    whitelist = leer_whitelist()
+    return f"<pre>{whitelist}</pre>"
 
 
 if __name__ in "__main__":
